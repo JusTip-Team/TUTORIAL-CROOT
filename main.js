74db86fd1ba74beb9d32c81f55295c9f2bbf9347
Azim <azim.wadallah@digitalbdg.ac.id>
2026-04-20 14:33:20 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,12 +5,11 @@ wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
 wauthparam.keyword="aHR0cHM6Ly93YS5tZS82MjgzMTMxODk1MDAwP3RleHQ9d2g0dDVhdXRoMA==";
 wauthparam.tokencookiehourslifetime=18;
 wauthparam.redirect ="/auth"
+
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam);
    
 const url="https://asia-southeast2-awangga.cloudfunctions.net/bukupedia/auth/users";
-
 const client_id="239713755402-4hr2cva377m43rsqs2dk0c7f7cktfeph.apps.googleusercontent.com";
 
-// Panggil fungsi untuk menambahkan elemen
 appendGoogleSignin(client_id,url);
